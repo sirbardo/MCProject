224695a5ad6294ab980e93ee8ee16430e46d8ddb
Laerte Frongia <bardo_italiano@hotmail.it>
2018-07-28 22:02:00 +0200

    just run the tests and it's done

--- tests.m
+++ tests.m
@@ -1,19 +1,14 @@
-n = 1000
+for n = [10 100 500 1000 2000 5000 10000] %different sizes
 
+n
+    
 A = rand(n);
+A = 100*A;
 A = A'*A;
 soluz = ones(n,1);
 b = A*soluz;
 
-%MATLAB
-tic;
-[Q, R] = qr(A);
-matlabqr_time = toc
-
-c = Q'*b;
-x = R\c;
-error_matlabqr = norm(x-soluz)
-
+%MATLAB QR
 tic;
 R = chol(A);
 matlabchol_time = toc
@@ -22,25 +17,15 @@ y = R'\b;
 x = R\y;
 error_matlabchol = norm(x-soluz)
 
-
-%HOUSEHOLDER
+%MATLAB CHOL
 tic;
-[Q, R] = QRHouseholderQuadrate(A);
-qrhouseholdertime = toc
+[Q, R] = qr(A);
+matlabqr_time = toc
+
 c = Q'*b;
 x = R\c;
-error_householder = norm(x-soluz)
+error_matlabqr = norm(x-soluz)
 
-%GIVENSDIRETTO
-%tic;
-%[Qgivens, Rgivens] = QRGivensQuadrateEsplicito(A);
-%qrgivensdirettotime = toc
-
-%GIVENSIMPLICITO
-
-tic;
-[Rgivensimpl, C, S] = QRGivensQuadrate(A);
-qrgivensimplicito = toc
 
 %CHOLESKY
 tic;
@@ -51,4 +36,34 @@ y = R'\b;
 x = R\y;
 error_chol = norm(x-soluz)
 
+%GIVENSIMPLICITO
+tic;
+[R, C, S] = QRGivensQuadrate(A);
+qrgivensimplicito = toc
+
+G = eye(n);
+for k = 1:n;
+    for i = k+1:n;
+        Gij = eye(n);
+        Gij(k,k) = C(k, i);
+        Gij(i,i) = C(k, i);
+        Gij(k,i) = S(k, i);
+        Gij(i,k) = -S(k, i);
+        G = Gij*G;
+    end
+end
+c = G*b;
+x = R\c;
+error_givens = norm(x-soluz)
+
+
+%HOUSEHOLDER
+tic;
+[Q, R] = QRHouseholderQuadrate(A);
+qrhouseholdertime = toc
+c = Q'*b;
+x = R\c;
+error_householder = norm(x-soluz)
+
+end
 
